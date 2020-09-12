27932ea1006ced28bbe91984f63cf603c1639beb
Arnd <Arnd_github@SV-Zanshin.com>
2020-09-12 11:07:31 +0200

    Update DisplayReadings.ino

--- examples/DisplayReadings/DisplayReadings.ino
+++ examples/DisplayReadings/DisplayReadings.ino
@@ -80,7 +80,7 @@
 **************************************************************************************************/
 const uint32_t SERIAL_SPEED    = 115200;  ///< Use fast serial speed
 const uint32_t SHUNT_MICRO_OHM = 100000;  ///< Shunt resistance in Micro-Ohm, e.g. 100000 is 0.1 Ohm
-const uint16_t MAXIMUM_AMPS    = 1;       ///< Max expected amps, values are 1 - clamped to max 1022
+const uint16_t MAXIMUM_AMPS    = 1;       ///< Max expected amps, clamped from 1A to a max of 1022A
 uint8_t        devicesFound    = 0;       ///< Number of INAs found
 INA_Class      INA;                       ///< INA class instantiation
 
